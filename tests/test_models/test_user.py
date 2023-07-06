6f92c2974c09e3706a787df471032f2b28250eff
MbaleeThingo <zinhle.mbali@yahoo.com>
2023-07-06 13:41:45 +0000

    k

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""test for state"""
+"""test for user"""
 import unittest
 import os
-from models.state import State
+from models.user import User
 from models.base_model import BaseModel
 import pep8
 
 
-class TestState(unittest.TestCase):
-    """this will test the State class"""
+class TestUser(unittest.TestCase):
+    """this will test the User class"""
 
     @classmethod
     def setUpClass(cls):
         """set up for test"""
-        cls.state = State()
-        cls.state.name = "CA"
+        cls.user = User()
+        cls.user.first_name = "Kevin"
+        cls.user.last_name = "Yook"
+        cls.user.email = "yook00627@gmamil.com"
+        cls.user.password = "secret"
 
     @classmethod
     def teardown(cls):
         """at the end of the test this will tear it down"""
-        del cls.state
+        del cls.user
 
     def tearDown(self):
         """teardown"""
@@ -28,39 +31,45 @@ class TestState(unittest.TestCase):
         except Exception:
             pass
 
-    def test_pep8_Review(self):
+    def test_pep8_User(self):
         """Tests pep8 style"""
         style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/state.py'])
+        p = style.check_files(['models/user.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_checking_for_docstring_State(self):
+    def test_checking_for_docstring_User(self):
         """checking for docstrings"""
-        self.assertIsNotNone(State.__doc__)
+        self.assertIsNotNone(User.__doc__)
 
-    def test_attributes_State(self):
-        """chekcing if State have attributes"""
-        self.assertTrue('id' in self.state.__dict__)
-        self.assertTrue('created_at' in self.state.__dict__)
-        self.assertTrue('updated_at' in self.state.__dict__)
-        self.assertTrue('name' in self.state.__dict__)
+    def test_attributes_User(self):
+        """chekcing if User have attributes"""
+        self.assertTrue('email' in self.user.__dict__)
+        self.assertTrue('id' in self.user.__dict__)
+        self.assertTrue('created_at' in self.user.__dict__)
+        self.assertTrue('updated_at' in self.user.__dict__)
+        self.assertTrue('password' in self.user.__dict__)
+        self.assertTrue('first_name' in self.user.__dict__)
+        self.assertTrue('last_name' in self.user.__dict__)
 
-    def test_is_subclass_State(self):
-        """test if State is subclass of BaseModel"""
-        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
+    def test_is_subclass_User(self):
+        """test if User is subclass of Basemodel"""
+        self.assertTrue(issubclass(self.user.__class__, BaseModel), True)
 
-    def test_attribute_types_State(self):
-        """test attribute type for State"""
-        self.assertEqual(type(self.state.name), str)
+    def test_attribute_types_User(self):
+        """test attribute type for User"""
+        self.assertEqual(type(self.user.email), str)
+        self.assertEqual(type(self.user.password), str)
+        self.assertEqual(type(self.user.first_name), str)
+        self.assertEqual(type(self.user.first_name), str)
 
-    def test_save_State(self):
+    def test_save_User(self):
         """test if the save works"""
-        self.state.save()
-        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+        self.user.save()
+        self.assertNotEqual(self.user.created_at, self.user.updated_at)
 
-    def test_to_dict_State(self):
+    def test_to_dict_User(self):
         """test if dictionary works"""
-        self.assertEqual('to_dict' in dir(self.state), True)
+        self.assertEqual('to_dict' in dir(self.user), True)
 
 
 if __name__ == "__main__":
